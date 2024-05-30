92a574c6dab1cc709a6233faaf5fa861353206c4
kadesai3 <kadesai3@myseneca.ca>
2024-05-30 11:42:55 -0400

    Update lab2a.py

--- lab2a.py
+++ lab2a.py
@@ -2,7 +2,7 @@
 
 name = 'Jon'
 age = 20 
-colour = input("Type in a colour and press enter: ")
+#colour = input("Type in a colour and press enter: ")
 
-# print('Hi ' + name + ', you are ' + str(age) + ' years old.')
-print('The colour I typed is ' + colour)
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+#print('The colour I typed is ' + colour)
